af98589c8af1c3051dd0371d38a34b4a01320f70
Gabe Hargrave <gkhargrave@gmail.com>
2018-10-06 18:05:47 -0700

    Move s3 image loading into separate component

--- src/Application.js
+++ src/Application.js
@@ -1,27 +1,51 @@
-import React, { Component } from 'react';
-import './Application.css';
+import React, { Component, PureComponent } from 'react'
+import './Application.css'
 
 import { Storage } from 'aws-amplify'
+
+class S3Image extends PureComponent {
+  state = { src: null }
+
+  async componentDidMount() {
+    const { s3key } = this.props
+    const src = await Storage.get(s3key)
+    this.setState({ src })
+  }
+
+  render() {
+    const { src } = this.state
+    if (!src) {
+      return null
+    }
+
+    return (
+      <article>
+        <img src={src} />
+      </article>
+    )
+  }
+}
 
 class Application extends Component {
   state = {
     files: []
-  };
+  }
 
   async componentDidMount() {
-    const keys = await Storage.list('')
-    const files = await Promise.all(keys.map(async ({ key }) => await Storage.get(key)))
+    // const keys = await Storage.list('')
+    // const files = await Promise.all(keys.map(async ({ key }) => await Storage.get(key)))
+    const files = await Storage.list('')
     this.setState({ files })
   }
 
-  handleSubmit = event => {
-    event.preventDefault();
+  handleSubmit = async (event) => {
+    event.preventDefault()
 
-    const file = this.fileInput.files[0];
-    const { name } = file;
-
-    console.log(file, name);
-  };
+    const file = this.fileInput.files[0]
+    const { name } = file
+    const newFile = await Storage.put(name, file)
+    this.setState(({ files }) => ({ files: files.concat(newFile) }))
+  }
 
   render() {
     return (
@@ -34,13 +58,13 @@ class Application extends Component {
           <input className="full-width" type="submit" />
         </form>
         <section className="Application-images">
-          {this.state.files.map((file, i) => {
-            return <img src={file} key={i} />
+          {this.state.files.map(({ key }, i) => {
+            return <S3Image s3key={key} key={key} />
           })}
         </section>
       </div>
-    );
+    )
   }
 }
 
-export default Application;
+export default Application
